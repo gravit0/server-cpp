7191ebb3c92649b212940bb882392cc69bbd3983
Gravit <xellgf2013@ya.ru>
2016-07-27 17:58:46 +0700

    Разработка добавлена в репозиторий

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
+using namespace std;
+namespace fs = boost::filesystem;
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
